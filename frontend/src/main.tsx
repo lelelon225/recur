2db62979c5ff207df9274bc90a522da795a61e6a
Leon <leon.hebeisen08@gmail.com>
2026-06-29 12:09:21 +0200

    [Add] atomic design folderstructure and base templates

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { StyledEngineProvider } from '@mui/material/styles'
+import { BrowserRouter } from 'react-router'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <StyledEngineProvider injectFirst>
+      <BrowserRouter>
+      <App />
+      </BrowserRouter>
+    </StyledEngineProvider>
   </StrictMode>,
 )
