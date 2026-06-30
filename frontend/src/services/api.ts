31213ecd7f7455e7d1f8b4cb5fc8da698c7ea704
JM2101 <jemuel.maurer@noseryoung.com>
2026-06-30 11:44:51 +0200

    [Added] service for Tasks

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,10 +1,8 @@
 import axios from "axios";
 
-axios.create({
-  baseURL: "http://localhost:3000/api",
-  headers: {
-    "Content-Type": "application/json",
-  },
+const api = axios.create({
+  baseURL: "http://localhost:8080",
+  timeout: 5000,
 });
 
-export default axios;
+export default api;
