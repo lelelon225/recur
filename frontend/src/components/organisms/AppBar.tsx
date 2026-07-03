1688632c1822b76c375016b5b908f34853a9c8c9
raffa <me@raffaelmaier.com>
2026-07-03 10:02:50 +0200

    [removed] Hamburgermenu removed

--- frontend/src/components/organisms/AppBar.tsx
+++ frontend/src/components/organisms/AppBar.tsx
@@ -1,49 +1,29 @@
-import {
-    AppBar as MuiAppBar,
-    Toolbar,
-    IconButton,
-    Box,
-} from "@mui/material";
+import { AppBar as MuiAppBar, Toolbar, Box } from "@mui/material";
 import type { AppBarProps as MuiAppBarProps } from "@mui/material/AppBar";
-import MenuIcon from "@mui/icons-material/Menu";
-import { useState } from "react";
-import AppBarDrawer from "../atoms/AppBarDrawer";
 
 type AppBarProps = MuiAppBarProps & {
-    children: React.ReactNode;
-    };
+  children: React.ReactNode;
+};
 
 function AppBar({ children, ...props }: AppBarProps) {
-    const [drawerOpen, setDrawerOpen] = useState(false);
-
-return (
-        <>
-        <MuiAppBar position="static" {...props}>
-            <Toolbar>
-            <Box
-                sx={{
-                width: "100%",
-                display: "flex",
-                justifyContent: "space-between",
-                alignItems: "center",
-                }}
-            >
-                <Box>{children}</Box>
-
-                <IconButton
-                color="inherit"
-                aria-label="menu"
-                onClick={() => setDrawerOpen(true)}
-                >
-                <MenuIcon sx={{ fontSize: "3rem" }} />
-                </IconButton>
-            </Box>
-            </Toolbar>
-        </MuiAppBar>
-        <AppBarDrawer drawerOpen={drawerOpen} setDrawerOpen={setDrawerOpen} className="appBarDrawer" />
-
-        </>
-    );
+  return (
+    <>
+      <MuiAppBar position="static" {...props}>
+        <Toolbar>
+          <Box
+            sx={{
+              width: "100%",
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+            {children}
+          </Box>
+        </Toolbar>
+      </MuiAppBar>
+    </>
+  );
 }
 
-    export default AppBar;
+export default AppBar;
--- frontend/src/components/templates/DefaulLayout.tsx
+++ frontend/src/components/templates/DefaulLayout.tsx
@@ -1,35 +1,45 @@
 import { useNavigate } from "react-router-dom";
-import NavigationBar from "../atoms/NavigationBar"
-import RestoreIcon from '@mui/icons-material/Restore';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import ArchiveIcon from '@mui/icons-material/Archive';
-import AppBar from "../organisms/AppBar"
+import NavigationBar from "../atoms/NavigationBar";
+import RestoreIcon from "@mui/icons-material/Restore";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import ArchiveIcon from "@mui/icons-material/Archive";
+import AppBar from "../organisms/AppBar";
 
 type DefaultLayoutProps = {
-  children: React.ReactNode
-}
-
+  children: React.ReactNode;
+};
 
 function DefaultLayout({ children }: DefaultLayoutProps) {
   const navigate = useNavigate();
 
-
- 
   return (
     <div className="default-layout">
-     <AppBar position="static" className="appBar">
+      <AppBar position="static" className="appBar">
         <h1>RECUR</h1>
       </AppBar>
-      <div className="content">
-        {children}
-      </div>
-      <NavigationBar className="navigationBar" destinations={[
-        { navigate: () => navigate('/'), label: "Recent", icon: <RestoreIcon /> },
-        { navigate: () => navigate('/favorites'), label: "Favorites", icon: <FavoriteIcon /> },
-        { navigate: () => navigate('/archive'), label: "Archive", icon: <ArchiveIcon /> },
-      ]} />
+      <div className="content">{children}</div>
+      <NavigationBar
+        className="navigationBar"
+        destinations={[
+          {
+            navigate: () => navigate("/"),
+            label: "Recent",
+            icon: <RestoreIcon />,
+          },
+          {
+            navigate: () => navigate("/favorites"),
+            label: "Favorites",
+            icon: <FavoriteIcon />,
+          },
+          {
+            navigate: () => navigate("/archive"),
+            label: "Archive",
+            icon: <ArchiveIcon />,
+          },
+        ]}
+      />
     </div>
-  )
+  );
 }
 
-export default DefaultLayout
+export default DefaultLayout;
