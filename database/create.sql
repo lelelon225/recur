aafff8c17b86bf7714584677ce6774622239364b
Leon Hebeisen <leon.hebeisen08@gmail.com>
2026-07-23 16:04:39 +0200

    [add] dummydata

--- database/create.sql
+++ database/create.sql
@@ -1,8 +1,20 @@
-/*CREATE DATABASE "recur";*/
+
 
 CREATE SCHEMA data;
 
 SET search_path TO data;
+
+CREATE TABLE app_user (
+    id UUID PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password_hash VARCHAR(255),
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255),
+    avatar_url VARCHAR(255),
+    provider VARCHAR(255) NOT NULL,
+    enabled BOOLEAN DEFAULT TRUE,
+    date_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 
 CREATE TABLE task (
     id UUID PRIMARY KEY,
@@ -18,20 +30,22 @@ CREATE TABLE task (
     is_favorite BOOLEAN DEFAULT FALSE,
     is_archived BOOLEAN DEFAULT FALSE,
     duration_minutes INTEGER DEFAULT 60,
-    start_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    start_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    user_id UUID REFERENCES data.app_user(id)
 );
 
-CREATE TABLE app_user (
-    id UUID PRIMARY KEY,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    password_hash VARCHAR(255),
-    first_name VARCHAR(255) NOT NULL,
-    last_name VARCHAR(255),
-    avatar_url VARCHAR(255),
-    provider VARCHAR(255) NOT NULL,
-    enabled BOOLEAN DEFAULT TRUE,
-    date_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+COPY data.app_user(
+    id,
+    email,
+    password_hash,
+    first_name,
+    last_name,
+    avatar_url,
+    provider,
+    enabled,
+    date_created 
+)
+FROM '/docker-entrypoint-initdb.d/app_user.csv' DELIMITER ',' CSV HEADER;
 
 COPY data.task (
     id,
@@ -46,6 +60,7 @@ COPY data.task (
     is_favorite,
     is_archived,
     duration_minutes,
-    start_time
+    start_time,
+    user_id
 )
-FROM '/docker-entrypoint-initdb.d/data.csv' DELIMITER ',' CSV HEADER;
+FROM '/docker-entrypoint-initdb.d/task.csv' DELIMITER ',' CSV HEADER;
