65789b6d4bfb05a1a423b0cac550d84114e16a03
Leon <leon.hebeisen08@gmail.com>
2026-09-24 09:18:06 +0200

    [Fixed] LoadingButton shrinking to icon width during loading state

--- frontend/src/components/atoms/loading/LoadingButton.tsx
+++ frontend/src/components/atoms/loading/LoadingButton.tsx
@@ -9,7 +9,8 @@ type LoadingButtonProps = ComponentProps<typeof Button> & {
 function LoadingButton({ loading, disabled, children, ...props }: LoadingButtonProps) {
   return (
     <Button disabled={disabled || loading} {...props}>
-      {loading ? <Loader2 className="h-4 w-4 animate-spin" /> : children}
+      {loading && <Loader2 className="h-4 w-4 animate-spin" />}
+      {children}
     </Button>
   );
 }
